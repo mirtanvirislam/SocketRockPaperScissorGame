a9c248496b5c6e8d0bf70e0864f4e3e6157ea664
Mir Tanvir Islam <tanvir.islam04@northsouth.edu>
2019-06-19 23:37:40 +0600

    Unstable multiptiple clients

--- Server.java
+++ Server.java
@@ -8,6 +8,8 @@ import java.util.Scanner;
 public class Server {
     private static ServerSocket server = null;
     private static Socket socket1 = null;
+    private static int s1Flag = 0;
+    private static int s2Flag = 0;
     private static Socket socket2 = null;
     private static final int port = 8080;
     public static void main(String[] args) {
@@ -16,6 +18,8 @@ public class Server {
         BufferedReader in2 = null;
         PrintWriter out1 = null;
         PrintWriter out2 = null;
+        String Client1Name="";
+        String Client2Name="";
         Scanner consoleInput = new Scanner(System.in);
 //Start Server
         try {
@@ -28,52 +32,75 @@ public class Server {
         } while(true) {
 //Create Socket
 
-            try {
-                socket1 = server.accept();
-                System.out.println("Client1 has been connected");
-            } catch (IOException e) {
-                System.out.println("Communication Error with client1");
-                        System.exit(-1);
-            }try {
-                socket2 = server.accept();
-                System.out.println("Client2 has been connected");
-            } catch (IOException e) {
-                System.out.println("Communication Error with client2");
-                        System.exit(-1);
+
+
+            if( s1Flag == 0 ) {
+                try {
+                    socket1 = null;
+                    socket1 = server.accept();
+                    System.out.println("Client1 has been connected");
+
+                } catch (IOException e) {
+                    System.out.println("Communication Error with client1");
+                    System.exit(-1);
+                }
+            }
+
+            if(s2Flag==0) {
+                try {
+                    socket2 = null;
+                    socket2 = server.accept();
+                    System.out.println("Client2 has been connected");
+
+                } catch (IOException e) {
+                    System.out.println("Communication Error with client2");
+                    System.exit(-1);
+                }
             }
 
             //communication with clients
             try {
 
+                System.out.println(" inside try soc1");
+
                 //communication with client 1
-                in1 = new BufferedReader(
-                        new InputStreamReader(
-                                socket1.getInputStream()
-                        )
-                );
-                out1 = new PrintWriter(socket1.getOutputStream(),
-                        true);
-                out1.println("CSE338 LAB Server");
-                String Client1Name = in1.readLine();
-                System.out.println("Client1 Name: " + Client1Name);
+                if(s1Flag==0) {
+                    in1 = new BufferedReader(
+                            new InputStreamReader(
+                                    socket1.getInputStream()
+                            )
+                    );
+
+                    System.out.println(" check in1 " + in1);
+
+                    out1 = new PrintWriter(socket1.getOutputStream(),
+                            true);
+                    out1.println("CSE338 LAB Server");
+                    Client1Name = in1.readLine();
+                    System.out.println("Client1 Name: " + Client1Name);
+                    s1Flag=1;
+                }
 
                 //communication with client 2
-                in2 = new BufferedReader(
-                        new InputStreamReader(
-                                socket2.getInputStream()
-                        )
-                );
-                out2 = new PrintWriter(socket2.getOutputStream(),
-                        true);
-                out2.println("CSE338 LAB Server");
-                String Client2Name = in2.readLine();
-                System.out.println("Client2 Name: " + Client2Name);
+                if(s2Flag==0) {
+                    in2 = new BufferedReader(
+                            new InputStreamReader(
+                                    socket2.getInputStream()
+                            )
+                    );
+                    out2 = new PrintWriter(socket2.getOutputStream(),
+                            true);
+                    out2.println("CSE338 LAB Server");
+                    Client2Name = in2.readLine();
+                    System.out.println("Client2 Name: " + Client2Name);
+                    s2Flag=1;
+                }
+
                 out1.println(Client1Name+ " vs " + Client2Name);
                 out2.println(Client1Name+ " vs " + Client2Name);
 
-                while(socket1.isConnected() && socket2.isConnected() && !socket1.isClosed() && !socket2.isClosed()) {
+                while(socket1.isConnected() && socket2.isConnected() ) {
                     System.out.print("\n\nServer: Game starts \n");
-
                     out1.println("Make your move - 1.Rock, 2.Paper, 3.Scissor 4.Exit game");
                     System.out.print("Client 1 ("+Client1Name+" ): ");
                     int Client1Move = Integer.parseInt(in1.readLine());
@@ -86,30 +113,17 @@ public class Server {
 
                     String Winner = ""; // Tracks winner
 
-                    //Exit game condition handler
-                    if ( Client1Move==4){
-                        System.out.println("Client 1 left");
-                        out1.println("Abandoned");
-                        out2.println("Abandoned");
-                        socket1.close();
-                        Winner = "Game Abandoned";
-                    }else if( Client2Move==4){
-                        System.out.println("Client 2 left");
-                        out1.println("Abandoned");
-                        out2.println("Abandoned");
-                        socket2.close();
-                        Winner = "Game Abandoned";
-                    }
+
 
                     //Game winner calculation
-                    else if ( Client1Move == Client2Move ){
+                    if ( Client1Move == Client2Move ){
                         Winner = " Draw";
                     }else if ( (Client1Move==2 && Client2Move ==1 ) || (Client1Move==1 && Client2Move ==3 ) || (Client1Move==3 && Client2Move ==2 ) ){
                         Winner = "Client 1 : "+ Client1Name;
                     }else if ( (Client2Move==2 && Client1Move ==1 ) || (Client2Move==1 && Client1Move ==3 ) || (Client2Move==3 && Client1Move ==2 ) ){
                         Winner = "Client 2 : "+ Client2Name;
                     }else {
-                        Winner = "Error";
+                        Winner = "Abandoned";
                     }
 
                     //Show game summary
@@ -118,14 +132,31 @@ public class Server {
                     out2.println(MovesSummary);
                     System.out.print(MovesSummary);
 
+                    //Exit game condition handler
+                    if ( Client1Move==4){
+                        System.out.println("Client 1 left");
+                        s1Flag=0;
+                        out2.println("Abandoned game");
+                        socket1.close();
+                        Winner = "Game Abandoned";
+                    }else if( Client2Move==4){
+                        System.out.println("Client 2 left");
+                        s2Flag=0;
+                        socket2.close();
+                        Winner = "Game Abandoned";
+                    }
+
                 }
 
             } catch (IOException e) {
                 if(!socket1.isConnected()){
-                    System.out.print("Client 1 left");
+                    System.out.println("--Client 1 left");
                 }if(!socket2.isConnected()){
-                    System.out.print("Client 2 left");
+                    System.out.println("--Client 2 left");
                 }
+
+                System.out.print("IO EXCEPTION -- ");
+                System.out.print(e);
 
                 consoleInput.close();
             }
